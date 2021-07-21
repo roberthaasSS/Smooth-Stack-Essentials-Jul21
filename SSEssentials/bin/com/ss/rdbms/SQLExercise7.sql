38d2e109367443ea4083ba0d2c0a68f4220bb0ed
roberthaas47 <45320995+roberthaas47@users.noreply.github.com>
2021-07-20 17:36:35 -0700

    Fixed SQLExcersice7

--- SSEssentials/bin/com/ss/rdbms/SQLExercise7.sql
+++ SSEssentials/bin/com/ss/rdbms/SQLExercise7.sql
@@ -1,3 +1,6 @@
 SELECT title, noOfCopies
-FROM library.tbl_author NATURAL JOIN library.tbl_book NATURAL JOIN library.tbl_book_copies NATURAL JOIN library.tbl_library_branch
-WHERE library.tbl_author.authorName = "Stephen King" AND library.tbl_library_branch.branchName = "Central"
+FROM library.tbl_book
+INNER JOIN library.tbl_author ON library.tbl_book.authId = library.tbl_author.authorId
+INNER JOIN library.tbl_book_copies ON library.tbl_book.bookId = library.tbl_book_copies.bookId
+INNER JOIN library.tbl_library_branch ON library.tbl_book_copies.branchId = library.tbl_library_branch.branchId
+WHERE library.tbl_author.authorName = 'Stephen King' AND library.tbl_library_branch.branchName = 'Central'
